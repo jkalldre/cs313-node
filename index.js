9e3e14cbec20893a11fabe425b71d2f962beb8d1
Jacob Alldredge <jakekalldredge@gmail.com>
2018-06-21 14:49:47 -0600

    db stuff

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')))
+   .set('views', path.join(__dirname, 'views'))
+   .set('view engine', 'ejs')
+   .get('/', (req, res) => res.render('pages/index'))
+   .get('/cool', (req, res) => res.send(cool()))
     .get('/db', async(req,res) => {
         try {
             const client = await pool.connect()
