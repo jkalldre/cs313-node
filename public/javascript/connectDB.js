f49ce5ff7e68895ffb0e4541bf0712311db1ae4f
Jacob Alldredge <jakekalldredge@gmail.com>
2018-06-27 16:41:25 -0600

    got local db working

--- public/javascript/connectDB.js
+++ public/javascript/connectDB.js
@@ -2,5 +2,5 @@ const pg = require('pg-promise')({});
 const PORT = process.env.PORT || 8000;
 const conString = `postgres://postgres:password@localhost:5432/forumdb`; //replace for heroku
 const db = pg(conString);
-console.log(process.env.DATABASE_URL);
+console.log("DATABASE_URL: " +process.env.DATABASE_URL);
 module.exports = db; //export
