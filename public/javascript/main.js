7d303a4c1dbaf3afa480860cc0a93f41f55af440
Jacob Alldredge <jakekalldredge@gmail.com>
2018-07-14 19:04:43 -0600

    cleaned code and added comments

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,13 +2,20 @@ console.log('main.js included.');
 let g_user = -1;
 let g_topic_id;
 
+/******************************************************************
+* authenticate: this function will handle the authentication of users.
+ * if user is authenticated getTopics will be called, otherwise an error
+ * is given.
+*******************************************************************/
 function authenticate() {
     var username = document.querySelector('#usrname').value;
     var password = document.querySelector('#usrpwd').value;
+    // authenticate endpoint
     fetch(`/authenticate?username=${username}&password=${password}`)
         .then(result => result.json())
         .then(json => {
             var authenticated = json['?column?'];
+            // if authenticated
             if (authenticated) {
                 fetch(`/userID?username=${username}`)
                     .then(result => result.json())
@@ -23,6 +30,7 @@ function authenticate() {
                     })
                     .catch(err => console.log(`authenticate cs error: ${err}`));
             }
+            // print error if failure to authenticate.
             else {
                 var error = document.querySelector('#error')
                 error.innerHTML = "Unable to authenticate. Username or password incorrect."
@@ -31,6 +39,9 @@ function authenticate() {
         .catch(err => console.log(`authenticate: ${err}`));
 }
 
+/******************************************************************
+* getTopics: fill html body with list of topics queried from db
+*******************************************************************/
 function getTopics() {
     document.querySelector('#contents').innerHTML = '';
     fetch(`/getTopics`)
@@ -43,18 +54,30 @@ function getTopics() {
         .catch(err => console.log(err));
 }
 
+/******************************************************************
+ * addTopic: add a topic to database using current user and content of
+ * textarea, and call getTopics to refresh html
+ *******************************************************************/
 function addTopic() {
     var textbox = document.querySelector('#newTopic').value;
     fetch(`/addTopic?id=${g_user}&content=${textbox}`);
     getTopics();
 }
 
+/******************************************************************
+ * deleteTopic: remove topic from database by id, and call getTopics
+ * to refresh html.
+ *******************************************************************/
 function deleteTopic() {
     var id = g_topic_id;
     fetch(`/deleteTopic?id=${id}`);
     getTopics();
 }
 
+/******************************************************************
+ * renderTopics: call renderTopic for each topic and add to html. Then
+ * add a button and text area for adding additional topics.
+ *******************************************************************/
 function renderTopics(results) {
     var objs = results.map(topic => {
         return renderTopic(topic);
@@ -77,6 +100,11 @@ function renderTopics(results) {
     div.appendChild(button);
 }
 
+/******************************************************************
+ * renderTopic: render an individial topic by adding an h2 and some data
+ * attributes used in queries. Button for deletion only added if topic
+ * was created by user.
+ *******************************************************************/
 function renderTopic(topic) {
     var div = document.createElement('div');
     var h2  = document.createElement('h2');
@@ -110,6 +138,9 @@ function renderTopic(topic) {
     return div;
 }
 
+/******************************************************************
+ * getComments: grab all comments associated with a topic.
+ *******************************************************************/
 function getComments(id,user) {
     fetch(`/getComments?id=${id}`)
         .then(results => {
@@ -121,22 +152,32 @@ function getComments(id,user) {
         .catch(err => console.log(err));
 }
 
+/******************************************************************
+ * openTopic: the onclick function that opens a topic and generates
+ * its comments depending on the topic_id
+ *******************************************************************/
 function openTopic(e) {
     var id = e.target.getAttribute('data-topic_id');
     var user = e.target.getAttribute('data-user_id');
-    var div = document.querySelector('#contents');
     getComments(id,user);
 }
 
+/******************************************************************
+ * addComment: add a comment to the currently selected topic. Call
+ * getComments to refresh html
+ *****************************************************************/
 function addComment(e) {
     var topic = e.target.getAttribute('data-topicID');
     var textbox = document.querySelector('#newComment').value;
     if (textbox != ''){
-    fetch(`/addComment?id=${g_user}&topic=${topic}&content=${textbox}`);
-    getComments(topic,g_user);
+        fetch(`/addComment?id=${g_user}&topic=${topic}&content=${textbox}`);
+        getComments(topic,g_user);
     }
 }
 
+/******************************************************************
+ * listComments: generates the html for the comments
+ *****************************************************************/
 function listComments(json,id,topicid) {
     var text = '';
     var objs = json.map(comment => {
@@ -182,6 +223,10 @@ function listComments(json,id,topicid) {
     div.appendChild(button2);
 }
 
+/******************************************************************
+ * deleteComment: deletes the selected comment then calls getComment
+ * to refresh html
+ *****************************************************************/
 function deleteComment(e) {
     var id = e.target.getAttribute('data-commentID');
     var user = e.target.getAttribute('data-userID');
@@ -190,6 +235,10 @@ function deleteComment(e) {
     getComments(topic,user);
 }
 
+/******************************************************************
+ * listComment: generate the html for a single comment. Button for
+ * deletion only added if comment was created by user.
+ *****************************************************************/
 function listComment(json,id,cID) {
     var div = document.createElement('div');
     var title = document.createTextNode(json.Content+" - "+json.cCreator);
